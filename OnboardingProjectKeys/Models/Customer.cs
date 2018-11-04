b2ea2106b56bb160c5ee24d9aa17d2017888e769
singh.barinder23 <singh.barinder23@outlook.com>
2018-11-05 10:25:44 +1300

    Add project files.

---
+++ OnboardingProjectKeys/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnboardingProjectKeys.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "Customer Name is required")]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Customer Address")]
+        public string Address { get; set; }
+      public virtual List<ProductSold> ProductSolds { get; set; }
+       // public virtual ICollection<ProductSold> ProductSolds { get; set; }
+    }
+}
---
+++ OnboardingProjectKeys/Models/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnboardingProjectKeys.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Store Name")]
+        [Required(ErrorMessage = "Store Name is required")]
+        [StringLength(100, MinimumLength = 2)]
+        public string StoreName { get; set; }
+        [Required]
+        [Display(Name = "Store Address")]
+        public string Address { get; set; }
+        public virtual List<ProductSold> ProductSolds { get; set; }
+       // public virtual ICollection<ProductSold> ProductSolds { get; set; }
+    }
+}
